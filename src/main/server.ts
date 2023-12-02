66259566eaa1d2ca85587fcda8838e13bf58751f
will-wyx <wyx_msn@hotmail.com>
2023-12-02 16:06:09 +0800

    feat(server): 支持批量打印

--- src/main/server.ts
+++ src/main/server.ts
@@ -39,7 +39,7 @@ router.get('/', (ctx: any) => {
   };
 });
 
-router.post('/print', async (ctx: any) => {
+router.post('/printOne', async (ctx: any) => {
   const url = ctx.request.body?.url;
 
   const printer = store.get('printer') as string;
@@ -75,6 +75,58 @@ router.post('/print', async (ctx: any) => {
         error,
       };
     }
+  } else {
+    ctx.response.body = {
+      success: false,
+      code: -3,
+      message: '未连接打印机',
+    };
+  }
+});
+
+/**
+ * 下载并打印
+ * @param url
+ * @param printer
+ */
+async function downloadAndPrint(url: string, printer: string) {
+  // 随机生成文件名
+  // eslint-disable-next-line no-use-before-define
+  const filename = GenNonDuplicateID();
+  try {
+    await download(url, `files`, {
+      filename,
+    });
+  } catch (error) {
+    // 下载失败
+    return { url, code: -2 };
+  }
+
+  try {
+    await print(`files\\${filename}`, { printer });
+  } catch (error) {
+    // 打印失败
+    return { url, code: -1 };
+  }
+
+  return { url, code: 0 };
+}
+
+router.post('/print', async (ctx: any) => {
+  const urls = ctx.request.body?.url;
+
+  const printer = store.get('printer') as string;
+  ctx.response.type = 'json';
+  if (printer) {
+    const promises = urls.map((url: string) => downloadAndPrint(url, printer));
+    const result = await Promise.all(promises);
+
+    ctx.response.body = {
+      success: true,
+      code: 0,
+      message: '成功',
+      result,
+    };
   } else {
     ctx.response.body = {
       success: false,
