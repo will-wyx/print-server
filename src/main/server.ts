bf31c15f3961817aeed3f9b599e930fd79e12791
will-wyx <wyx_msn@hotmail.com>
2023-12-07 10:22:39 +0800

    feat(server): 修改打印顺序

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,15 +1,15 @@
-import Koa from 'koa';
+import Koa from "koa";
 // @ts-ignore
-import Router from '@koa/router';
+import Router from "@koa/router";
 // import { koaBody } from 'koa-body';
-import bodyParser from 'koa-bodyparser';
+import bodyParser from "koa-bodyparser";
 // @ts-ignore
-import download from 'download';
+import download from "download";
 // @ts-ignore
-import { print } from 'pdf-to-printer';
+import { print } from "pdf-to-printer";
 // @ts-ignore
-import Store from 'electron-store';
-import fs from 'fs';
+import Store from "electron-store";
+import fs from "fs";
 
 const store = new Store();
 
@@ -18,7 +18,7 @@ const router = new Router();
 
 // 清空文件目录
 // eslint-disable-next-line no-use-before-define
-rmdir('files');
+rmdir("files");
 
 function rmdir(path: any) {
   if (fs.existsSync(path)) {
@@ -30,34 +30,34 @@ function rmdir(path: any) {
   }
 }
 
-router.get('/', (ctx: any) => {
-  ctx.response.type = 'json';
+router.get("/", (ctx: any) => {
+  ctx.response.type = "json";
   ctx.response.body = {
     success: true,
     code: 0,
-    message: '成功',
+    message: "成功"
   };
 });
 
-router.post('/print', async (ctx: any) => {
+router.post("/print", async (ctx: any) => {
   const url = ctx.request.body?.url;
 
-  const printer = store.get('printer') as string;
-  ctx.response.type = 'json';
+  const printer = store.get("printer") as string;
+  ctx.response.type = "json";
   if (printer) {
     // 随机生成文件名
     // eslint-disable-next-line no-use-before-define
     const filename = GenNonDuplicateID();
     try {
       await download(url, `files`, {
-        filename,
+        filename
       });
     } catch (error) {
       ctx.response.body = {
         success: false,
         code: -2,
-        message: '下载失败',
-        error,
+        message: "下载失败",
+        error
       };
     }
     try {
@@ -65,21 +65,21 @@ router.post('/print', async (ctx: any) => {
       ctx.response.body = {
         success: true,
         code: 0,
-        message: '成功',
+        message: "成功"
       };
     } catch (error: any) {
       ctx.response.body = {
         success: false,
         code: -1,
-        message: '打印失败',
-        error,
+        message: "打印失败",
+        error
       };
     }
   } else {
     ctx.response.body = {
       success: false,
       code: -3,
-      message: '未连接打印机',
+      message: "未连接打印机"
     };
   }
 });
@@ -95,7 +95,7 @@ async function downloadAndPrint(url: string, printer: string) {
   const filename = GenNonDuplicateID();
   try {
     await download(url, `files`, {
-      filename,
+      filename
     });
   } catch (error) {
     // 下载失败
@@ -112,26 +112,33 @@ async function downloadAndPrint(url: string, printer: string) {
   return { url, code: 0 };
 }
 
-router.post('/printMulti', async (ctx: any) => {
+async function printAll(urls: string[], printer: string) {
+  const data = [];
+  for (let i = 0; i < urls.length; i++) {
+    data.push(await downloadAndPrint(urls[i], printer));
+  }
+  return data;
+}
+
+router.post("/printMulti", async (ctx: any) => {
   const urls = ctx.request.body?.url;
 
-  const printer = store.get('printer') as string;
-  ctx.response.type = 'json';
+  const printer = store.get("printer") as string;
+  ctx.response.type = "json";
   if (printer) {
-    const promises = urls.map((url: string) => downloadAndPrint(url, printer));
-    const result = await Promise.all(promises);
+    const result = await printAll(urls, printer);
 
     ctx.response.body = {
       success: true,
       code: 0,
-      message: '成功',
-      result,
+      message: "成功",
+      result
     };
   } else {
     ctx.response.body = {
       success: false,
       code: -3,
-      message: '未连接打印机',
+      message: "未连接打印机"
     };
   }
 });
@@ -142,9 +149,9 @@ function GenNonDuplicateID(): String {
 
 koa
   .use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
-    ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.set("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
     await next();
   })
   // .use(koaBody())
